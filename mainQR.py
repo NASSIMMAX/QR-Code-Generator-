731403cf0730b71f4c3c616f996dd08e0b31a604
NASSIMMAX <wintmurt@gmail.com>
2024-07-05 22:09:36 +0100

    create a QR code fast

---
+++ mainQR.py
@@ -0,0 +1,31 @@
+import qrcode
+import base64
+import webbrowser
+import time
+
+
+textData = input('enter your text : '.capitalize())
+fileName = input('filename : '.capitalize())
+pathFile = f"imgQR/{fileName}.png"
+
+
+def makeQR(data, name):
+    codeQr = qrcode.make(data)
+    codeQr.save(name)
+
+
+makeQR(textData, pathFile)
+
+with open(pathFile, "rb") as imgCodeQr:
+    dataQR = base64.b64encode(imgCodeQr.read()).decode('utf-8')
+
+    with open("webQR.html", "w") as FileHTML:
+        FileHTML.write(f"""
+                               <html>
+                               <body>
+                               <img src='data:image/png;base64,{dataQR}'/>
+                               </body>
+                               </html>
+                               """)
+        htmlFile = "webQR.html"
+        webbrowser.open(htmlFile)
